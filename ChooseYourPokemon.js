3f644454de574df5c43f3ff680dc7c145147a9ac
XENONDK <dch3005@gmail.com>
2023-03-14 13:03:11 +0100

    added pokemon pictures and boxes on index

--- ChooseYourPokemon.js
+++ ChooseYourPokemon.js
@@ -7,12 +7,31 @@ fetch("https://pokeapi.co/api/v2/pokemon")
 })
 .then(function (data){
     data.results.forEach(Pokemon => {
-        let container = document.createElement("div")
+        
         let link = document.createElement("a")
-        container.innerHTML = Pokemon.name
+        let container = document.createElement("div")
+        let image = document.createElement("img")
+        let h2 = document.createElement("h2")
+        
         link.href = `details.html?name=${Pokemon.name}`
-        link.innerHTML = "view more"
-        container.append(link)
-        document.querySelector(".main").append(container)
+        
+        h2.innerHTML = Pokemon.name
+        
+        
+        fetch(Pokemon.url)
+        .then((response) => response.json())
+        .then((pokemonDetails) => 
+            image.src = pokemonDetails.sprites.front_default 
+            
+            )
+            
+            
+            container.append(image)
+        container.append(h2)
+        link.append(container)
+        document.querySelector(".main").append(link)
+
+        
+
 });
 })
--- pokemon.js
+++ pokemon.js
@@ -2,3 +2,5 @@ let params = new URLSearchParams(document.location.search)
 let identifier = params.get("name")
 
 document.querySelector("title").innerHTML = identifier
+
+fetch(`https://pokeapi.co/api/v2/pokemon/${identifier}`)
