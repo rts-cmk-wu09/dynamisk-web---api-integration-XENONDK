b30e01610db5ea73e167dfd55941302662e598fb
XENONDK <dch3005@gmail.com>
2023-03-16 12:52:59 +0100

    added pokemon details

--- pokemon.js
+++ pokemon.js
@@ -3,4 +3,18 @@ let identifier = params.get("name")
 
 document.querySelector("title").innerHTML = identifier
 
+
+let pokeDetail = document.querySelector("#details")
+console.log(pokeDetail)
+
 fetch(`https://pokeapi.co/api/v2/pokemon/${identifier}`)
+.then((Response) => Response.json())
+ .then((pokemonStats) =>  
+
+ pokeDetail.innerHTML = `   
+ <h1>${pokemonStats.name}</h1>
+    <p> Base experience: ${pokemonStats.base_experience}</p>
+     <p> ${pokemonStats.abilities[0].ability.name} </p>
+`
+
+ )
